1bbf7d917086d918dc8cdc0ba9d62d1c66d1b2d2
Oat Chana <chanathip0944729842@gmail.com>
2024-10-30 14:26:34 +0700

    feat:add link at title abd copy link button and search with tag

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import copyLinkIcon from "../assets/copyLinkIcon.png";
 
 function Article() {
   const [inputLocation, setInputLocation] = useState("");
@@ -11,22 +12,45 @@ function Article() {
         `http://localhost:4001/trips?keywords=${inputLocation}`
       );
       setSortedLocation(locations.data.data);
-      console.log(locations.data.data);
+      console.log(inputLocation);
+      console.log(sortedLocation)
     } catch {
       alert("can not get data");
     }
   };
 
+  const handleTagButton = (tag) => {
+    const newInputLocation = `${inputLocation} ${tag}`.trim();
+    setInputLocation(newInputLocation);
+  };
+
+  const copyToClipboard = async (url) => {
+    try {
+      await navigator.clipboard.writeText(url);
+      alert("Link copied to clipboard!");
+    } catch {
+      alert("Failed to copy link.");
+    }
+  };
+
   const LocationCard = ({ location }) => {
     return (
-      <div className="location-card flex flex-row gap-10 mx-10 ">
+      <div className="location-card flex gap-10 mx-10">
         <img
-          className="major-image w-[360px] h-[240px] rounded-xl object-cover"
+          className="major-image w-[480px] h-[240px] rounded-xl object-cover overflow-hidden"
           src={location.photos[0]}
           alt={location.title}
         ></img>
-        <div className="detail flex flex-col gap-3">
-          <h1 className="title text-[24px] font-[600]">{location.title}</h1>
+        <div className="detail flex flex-col gap-3 w-full">
+          <div className="title text-[24px] font-[600]">
+            <a
+              className="more-detail-button"
+              href={location.url}
+              target="_blank"
+            >
+              {location.title}
+            </a>
+          </div>
           <p className="description text-gray-500">
             {location.description.slice(0, 100)}...
           </p>
@@ -42,21 +66,31 @@ function Article() {
           <p className="text-gray-500">
             หมวด{" "}
             {location.tags.slice(0, location.tags.length - 1).map((tag) => (
-              <span className="underline mr-1 ml-1">{tag}</span>
+              <button
+                className="underline mr-1 ml-1"
+                onClick={() => handleTagButton(tag)}
+              >
+                {tag}
+              </button>
             ))}
             และ{" "}
-            <span className="underline mr-1 ml-1">
+            <button className="underline mr-1 ml-1" onClick={() => handleTagButton(location.tags[location.tags.length - 1])}>
               {location.tags[location.tags.length - 1]}
-            </span>
+            </button>
           </p>
-          <div className="minor-image flex gap-5">
-            {location.photos.slice(1, 4).map((photo) => (
-              <img
-                src={photo}
-                alt="minor-photo"
-                className="w-20 h-20 rounded-xl object-cover"
-              ></img>
-            ))}
+          <div className="flex justify-between">
+            <div className="minor-image flex gap-5">
+              {location.photos.slice(1, 4).map((photo) => (
+                <img
+                  src={photo}
+                  alt="minor-photo"
+                  className="w-20 h-20 rounded-xl object-cover"
+                ></img>
+              ))}
+            </div>
+            <button onClick={() => copyToClipboard(location.url)}>
+              <img src={copyLinkIcon} className="w-10 h-10"></img>
+            </button>
           </div>
         </div>
       </div>
